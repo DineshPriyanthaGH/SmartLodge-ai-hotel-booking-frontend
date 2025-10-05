24abdae4f535e923b6906c0cc46f6d6a80506a0a
Dinesh Priyantha <priyanthadinesh2002@gmail.com>
2025-10-05 14:00:53 +0530

    Add routing for Checkout and Booking Success components

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import { Star, MapPin, Award, Users, Heart, Camera } from 'lucide-react';
 
 
 import HomePage from './components/HomePage';
+import Checkout from './components/Checkout';
+import BookingSuccess from './components/BookingSuccess';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/hotel/:id" element={<HotelDetail />} />
+        <Route path="/checkout/:id" element={<Checkout />} />
+        <Route path="/booking-success" element={<BookingSuccess />} />
       </Routes>
       <Footer />
     </>
